5390d5f01d8b57f0b9b6941eb5ec891a9080dcd3
Matthias Wolf <matthias.wolf@epfl.ch>
2023-06-19 10:34:07 +0200

    BBPP134-417 Add required bits to the recipe.

--- fz_td_recipe/parts/common.py
+++ fz_td_recipe/parts/common.py
@@ -2,10 +2,14 @@
 
 NODE_FIELDS = {
     "fromEType": "*",
+    "fromHemisphere": "*",
+    "fromLayer": "*",
     "fromMType": "*",
     "fromRegion": "*",
     "fromSClass": "*",
     "toEType": "*",
+    "toHemisphere": "*",
+    "toLayer": "*",
     "toMType": "*",
     "toRegion": "*",
     "toSClass": "*",
