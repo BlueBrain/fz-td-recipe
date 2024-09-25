7c57ed27834a86f999a29c3e7ca3585e0b3050b8
Matthias Wolf <matthias.wolf@epfl.ch>
2024-09-25 11:54:15 +0200

    Add constraints to synapse property inputs.
    Having these values be zero or smaller will lead to non-sensical
    probability distribution inputs, either for the standard deviation of
    all distributions, or for the scale of the gamma distribution.
    
    Fixes #9

--- tests/test_synapse_property.py
+++ tests/test_synapse_property.py
@@ -1,18 +1,89 @@
 """Test synapse property mapping"""
 
+import json
 from io import StringIO
 
-from fz_td_recipe import XMLRecipe
+import pytest
+from jsonschema.exceptions import ValidationError
+
+from fz_td_recipe import Recipe, XMLRecipe
 
 
 def test_synapse_properties():
-    r = XMLRecipe(StringIO(RECIPE))
+    r = XMLRecipe(StringIO(RECIPE_XML))
     assert set(r.synapse_properties.rules.required) == set(
         ["fromRegion", "fromHemisphere", "toRegion", "toHemisphere"]
     )
 
 
-RECIPE = """\
+def test_valid_synapse_properties(circuit_config, tmp_path, parameter):
+    data = json.loads(RECIPE_JSON)
+
+    recipe_file = tmp_path / "recipe.json"
+    with recipe_file.open("w") as fd:
+        json.dump(data, fd)
+    Recipe(recipe_file, circuit_config, (None, None))
+
+
+@pytest.mark.parametrize(
+    "parameter",
+    [
+        "decay_time_sd",
+        "u_syn_sd",
+        "depression_time_mu",
+        "depression_time_sd",
+        "facilitation_time_mu",
+        "facilitation_time_sd",
+        "conductance_mu",
+        "conductance_sd",
+    ],
+)
+def test_invalid_synapse_properties(circuit_config, tmp_path, parameter):
+    data = json.loads(RECIPE_JSON)
+    data["synapse_properties"]["classes"][0][parameter] = 0.0
+
+    recipe_file = tmp_path / "recipe.json"
+    with recipe_file.open("w") as fd:
+        json.dump(data, fd)
+    with pytest.raises(ValidationError):
+        Recipe(recipe_file, circuit_config, (None, None))
+
+
+RECIPE_JSON = """\
+{
+  "bouton_distances": {},
+  "gap_junction_properties": {},
+  "seed": 0,
+  "synapse_properties": {
+    "rules": [
+      {
+        "src_mtype": "*",
+        "class": "I1"
+      }
+    ],
+    "classes": [
+      {
+        "class": "I1",
+        "n_rrp_vesicles_mu": 1,
+        "conductance_mu": 0.0,
+        "conductance_sd": 0.1,
+        "decay_time_mu": 8.3,
+        "decay_time_sd": 2.2,
+        "u_syn_mu": 0.25,
+        "u_syn_sd": 0.13,
+        "depression_time_mu": 706.0,
+        "depression_time_sd": 405.0,
+        "facilitation_time_mu": 21.0,
+        "facilitation_time_sd": 9.0
+      }
+    ]
+  },
+  "version": 1
+}
+"""
+
+
+RECIPE_XML = """\
 <?xml version="1.0"?>
 <blueColumn>
   <SynapsesProperties>
